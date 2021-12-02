917832fd52f965ea23dca4c46b6948be56cb4c77
tomascalvo <tomasrcalvo@gmail.com>
2021-12-01 23:52:44 -0500

    Put static assets, images, and stylesheet in public directory for netlify deployment.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import "./style.css";
+import "./public/style.css";
 
 const scene = new THREE.Scene();
 
